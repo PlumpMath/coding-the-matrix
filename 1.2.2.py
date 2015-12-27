ede0e77bf9e9965de760210e768b0afa64c8b9d8
bakyeono <bakyeono@gmail.com>
2015-12-27 19:56:07 +0900

    solve -1.2

---
+++ 1.2.2.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Quiz 1.2.2: Example 1.2.1의 A x B의 크기는?
+
+a = {'1', '2', '3'}
+b = {'a', 'b', 'c', 'd'}
+cartesian = {'1a', '1b', '1c', '1d', '2a', '2b', '2c', '2d', '3a', '3b', '3c', '3d'}
+
+print len(cartesian)
+
