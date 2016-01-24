847e6f3ec091e0ca846bba747ea63019fbdd0e2d
bakyeono <bakyeono@gmail.com>
2016-01-24 21:37:39 +0900

    solve 1.5

---
+++ 1.5.5.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# Task 1.5.5
+from fractions import Fraction
+
+S = {1, 2, 3, 4, 5}
+S2 = {x*x for x in S}
+
+print (S2)
+
+
---
+++ 1.5.6.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# Task 1.5.6
+from fractions import Fraction
+
+S = {0, 1, 2, 3, 4}
+S2 = {2**x for x in S}
+
+print (S2)
+
+
