1515f35e15eebadd193597145f8be72bbad8b99c
bakyeono <bakyeono@gmail.com>
2016-01-31 21:37:02 +0900

    solve 1.5

---
+++ 1.5.11.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# Task 1.5.11
+
+L1 = ['A', 'B', 'C']
+L2 = [1, 2, 3]
+
+cartesian = [[e1, e2] for e1 in L1 for e2 in L2]
+
+print (cartesian)
+
---
+++ 1.5.12.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Task 1.5.12
+
+LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
+
+total = sum(sum([L for L in LofL], []))
+
+print (total)
+
